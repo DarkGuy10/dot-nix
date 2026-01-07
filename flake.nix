74c1a06f3bd4bd7fe94ad5feb73eee229643bb5b
DarkGuy10 <SSSCerealSoup@proton.me>
2026-01-07 08:15:28 +0530

    feat: add catppuccin flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    catppuccin.url = "github:catppuccin/nix/release-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,16 +11,19 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, home-manager, ... }:
+    inputs@{
+      nixpkgs,
+      catppuccin,
+      home-manager,
+      ...
+    }:
     {
       nixosConfigurations = {
         eva-04 =
           let
             username = "cerealsoup";
-            hostname = "eva-04";
             specialArgs = {
               inherit username;
-              inherit hostname;
             };
           in
           nixpkgs.lib.nixosSystem {
@@ -28,6 +32,7 @@
             modules = [
               ./hosts/eva-04
               ./users/${username}/nixos.nix
+              catppuccin.nixosModules.catppuccin
 
               # make home-manager as a module of nixos
               # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
@@ -38,7 +43,12 @@
                 home-manager.backupFileExtension = "backup";
 
                 home-manager.extraSpecialArgs = inputs // specialArgs;
-                home-manager.users.${username} = import ./users/${username}/home.nix;
+                home-manager.users.${username} = {
+                  imports = [
+                    ./users/${username}/home.nix
+                    catppuccin.homeModules.catppuccin
+                  ];
+                };
               }
             ];
           };
